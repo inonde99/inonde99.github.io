b87927aeb5fa7616e681136fbb8686e40aceb487
Myeonghoon Kim <inonde@gmail.com>
2023-10-15 17:37:43 +0900

    Background 이미지 추가. Login 스타일 변경.

--- js/background.js
+++ js/background.js
@@ -1,10 +1,7 @@
 const images = [
-"0.jpeg", "1.jpeg", "2.jpeg"
+"0.jpeg", "1.jpeg", "2.jpg", "3.jpg", "4.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url('img/${chosenImage}')`; 
